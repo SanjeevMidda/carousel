dd7443185ffcdf147e07ee9f847610ff85b10cb1
Sanjeev Midda <s.midda1@gmail.com>
2026-02-18 20:44:16 +0000

    imported imgs into App.js and created UI for imgs and created directional arrows

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
 import "./index.css";
 
+import one from "./imgs/one.jpg";
+import two from "./imgs/two.jpg";
+import three from "./imgs/three.jpg";
+import four from "./imgs/four.jpg";
+import five from "./imgs/five.jpg";
+import six from "./imgs/six.jpg";
+import seven from "./imgs/seven.jpg";
+
 function App() {
-  return <div className="App"></div>;
+  // arrows
+  // get imgs
+  // container to hold our images
+  // smaller and bigger when in focus
+
+  return (
+    <div className="App">
+      <div className="leftArrow">{`<--`}</div>
+      <div className="mainImageContainer">
+        <img src={one} alt="" />
+        <img src={two} alt="" />
+        <img src={three} alt="" />
+        <img src={four} alt="" />
+        <img src={five} alt="" />
+        <img src={six} alt="" />
+        <img src={seven} alt="" />
+      </div>
+      <div className="rightArrow">{`-->`}</div>
+    </div>
+  );
 }
 
 export default App;
